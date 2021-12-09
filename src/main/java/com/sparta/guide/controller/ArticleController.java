c46cdcf5ebd7db7bc26e4672e76ec635340d500a
ho <jason.uphiller@gmail.com>
2021-12-09 16:22:26 +0900

    환경변수 테스트

--- src/main/java/com/sparta/guide/controller/ArticleController.java
+++ src/main/java/com/sparta/guide/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.sparta.guide.dto.PostArticleDto;
 import com.sparta.guide.dto.GetArticlesDto;
 import com.sparta.guide.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ArticleController {
@@ -29,6 +31,7 @@ public class ArticleController {
 
     @GetMapping("/articles")
     public List<GetArticlesDto.Response> getArticles(){
+        log.error("1234");
         List<Article> articles = articleService.getArticles();
         List<GetArticlesDto.Response> response = modelMapper.map(articles, new TypeToken<List<GetArticlesDto.Response>>() {}.getType());
         return response;
