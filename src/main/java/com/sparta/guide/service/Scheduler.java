7fb10631d44a143c6b6d50075752425389e95952
ho <jason.uphiller@gmail.com>
2021-12-29 17:47:22 +0900

    스케줄러 테스트

---
+++ src/main/java/com/sparta/guide/service/Scheduler.java
@@ -0,0 +1,18 @@
+package com.sparta.guide.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class Scheduler {
+    @Scheduled(cron = "0 55 17 ? * *")
+    public void runScheudler(){
+        log.error("스케줄러 동작 " + new Date());
+    }
+}
